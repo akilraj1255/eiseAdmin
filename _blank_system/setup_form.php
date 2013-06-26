fa2f35b17bff895d190e6a5794ffb247a4305e26
Ilya Eliseev <ie@e-ise.com>
2013-06-26 11:57:00 +0400

    _blank_system: some missing files added

--- _blank_system/setup_form.php
+++ _blank_system/setup_form.php
@@ -6,14 +6,21 @@ switch ($_POST["DataAction"]) {
   case "update":
        $rsSTP = $oSQL->do_query("SELECT * FROM stbl_setup");
        while ($rwSTP = $oSQL->fetch_array($rsSTP)) {
-          /* перебираем весь рекордсет и апдейтим записи одну за одной */
-          /* к переменным обращаться нельзя - они переинициализовались */
-          if (($_POST[$rwSTP["stpVarName"]]!=$rwSTP["stpCharValue"])&&($rwSTP["stpFlagReadOnly"]!="y")){
-              $sqlUpdSetupRow = "UPDATE tbl_setup SET stpCharValue=".$oSQL->escape_string($_POST[$rwSTP["stpVarName"]]).
-                                " WHERE stpVarName = '".$rwSTP["stpVarName"]."'";
-              $oSQL->do_query($sqlUpdSetupRow);
-          }
-       }
+            /* перебираем весь рекордсет и апдейтим записи одну за одной */
+            /* к переменным обращаться нельзя - они переинициализовались */
+            if (!$rwSTP["stpFlagReadOnly"]){
+                if($_POST[$rwSTP["stpVarName"]]!=$rwSTP["stpCharValue"]){
+                    $sqlUpdSetupRow = "UPDATE stbl_setup SET stpCharValue=".$oSQL->escape_string($_POST[$rwSTP["stpVarName"]]).
+                                    " WHERE stpVarName = '".$rwSTP["stpVarName"]."'";
+                    $oSQL->do_query($sqlUpdSetupRow);
+                }
+                if(!is_null($rwSTP["stpCharValueLocal"]) && $_POST[$rwSTP["stpVarName"]."_local"]!=$rwSTP["stpCharValueLocal"]){
+                    $sqlUpdSetupRow = "UPDATE stbl_setup SET stpCharValueLocal=".$oSQL->escape_string($_POST[$rwSTP["stpVarName"]."_local"]).
+                                    " WHERE stpVarName = '".$rwSTP["stpVarName"]."'";
+                    $oSQL->do_query($sqlUpdSetupRow);
+                }
+            }
+        }
        
        SetCookie("UserMessage", "Settings are saved");
        header("Location: ".$_SERVER["PHP_SELF"]);
@@ -29,26 +36,40 @@ include eiseIntraAbsolutePath."inc-frame_top.php";
 <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" class="eiseIntraForm">
 <input type="hidden" name="DataAction" value="update">
 
-<fieldset><legend><?php  echo $intra->translate("System settings"); ; ?></legend>
+<fieldset style="width: 66%;"><legend><?php  echo $intra->translate("System settings"); ?></legend>
 
 <?php
+    
+
    $rsSTP = $oSQL->do_query("SELECT * FROM stbl_setup ORDER BY stpFlagReadOnly ASC, stpNGroup ASC, stpID ASC");
    
    while ($rwSTP = $oSQL->fetch_array($rsSTP)) {
    
+    if (isset($oldGroup) && $oldGroup != $rwSTP["stpNGroup"])
+        echo "<hr>";
+   
    //print_r($rwSTP);
 ?>
-<div><label><?php echo $rwSTP["stpCharName"]; ?>:&nbsp;</label>
+<div class="eiseIntraField"><label><?php echo $rwSTP["stpTitle{$intra->local}"]; ?>:&nbsp;</label>
 	<?php 
-    $arrConf = Array("FlagWrite"=>(!$rwSTP["stpFlagReadOnly"] && (bool)$intra->arrUsrData["FlagWrite"])
-                , "class"=>"eiseIntraValue");
-    if ($rwSTP["stpCharType"]=="text")
-       echo $intra->showTextArea(htmlspecialchars($rwSTP["stpVarName"]), $rwSTP["stpCharValue"], array_merge($arrConf, Array("strAttrib"=>"rows=\"6\""))); 
-    else 
-       echo $intra->showTextBox(htmlspecialchars($rwSTP["stpVarName"]), $rwSTP["stpCharValue"], $arrConf); 
+    $arrConf = Array("FlagWrite"=>(!$rwSTP["stpFlagReadOnly"] && (bool)$intra->arrUsrData["FlagWrite"]));
+    if ($rwSTP["stpCharType"]=="text"){ 
+        echo $intra->showTextArea(htmlspecialchars($rwSTP["stpVarName"]), $rwSTP["stpCharValue"], array_merge($arrConf, Array("strAttrib"=>"rows=\"4\""))); 
+        if (!is_null($rwSTP["stpCharValueLocal"])){
+            echo $intra->showTextArea(htmlspecialchars($rwSTP["stpVarName"])."_local", $rwSTP["stpCharValueLocal"]
+            , array_merge($arrConf, Array("strAttrib"=>"rows=\"4\"", "class"=>"eiseIntraValue_next"))); 
+        }
+    } else {
+        echo $intra->showTextBox(htmlspecialchars($rwSTP["stpVarName"]), $rwSTP["stpCharValue"], $arrConf); 
+        if (!is_null($rwSTP["stpCharValueLocal"])){
+            echo $intra->showTextBox(htmlspecialchars($rwSTP["stpVarName"])."_local", $rwSTP["stpCharValueLocal"]
+                , array_merge($arrConf, Array("class"=>"eiseIntraValue_next"))); 
+        }
+    }
     ?>
 </div>      
 <?php
+        $oldGroup = $rwSTP["stpNGroup"];
    }
 ?>
 <script language="JavaScript">
@@ -58,7 +79,7 @@ function SubmitForm(){
 </script>
 
 <?php if ($intra->arrUsrData["FlagWrite"]) { ?>  
-<div><label>&nbsp;</label><input type="submit" value="Save" onClick="return SubmitForm();"></div>
+<div><label>&nbsp;</label><input type="submit" value="Save" class="eiseIntraSubmit" onClick="return SubmitForm();"></div>
 <?php } ?>
 
 </fieldset>
